59f342d75badeda7ad2fcdd54e70127bed8d1666
cataclysm1987 <conklin.eric@gmail.com>
2017-04-22 14:23:58 -0400

    add database 1 to 1 numbers to applicationusers

--- PregnancySMS/Models/IdentityModels.cs
+++ PregnancySMS/Models/IdentityModels.cs
@@ -9,6 +9,7 @@ namespace PregnancySMS.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual Number Number { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +21,8 @@ namespace PregnancySMS.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Number> Numbers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -28,6 +31,17 @@ namespace PregnancySMS.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Number>()
+                .HasOptional(pi => pi.ApplicationUser)
+                .WithRequired(lu => lu.Number);
+
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(l => l.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
         }
     }
 }
---
+++ PregnancySMS/Models/Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PregnancySMS.Models
+{
+    public class Number
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? Weeks { get; set; }
+        public bool HasDoctor { get; set; }
+        public bool HasInsurance { get; set; }
+        public bool HasNutrition { get; set; }
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
