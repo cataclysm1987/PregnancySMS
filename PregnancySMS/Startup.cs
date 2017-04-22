2845c07a5d798b1047052e3f89e46811787c94e1
cataclysm1987 <conklin.eric@gmail.com>
2017-04-22 12:39:07 -0400

    add registry in startup cs file

--- PregnancySMS/Startup.cs
+++ PregnancySMS/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using FluentScheduler;
+using Microsoft.Owin;
 using Owin;
+using PregnancySMS.Models;
 
 [assembly: OwinStartupAttribute(typeof(PregnancySMS.Startup))]
 namespace PregnancySMS
@@ -9,6 +11,21 @@ namespace PregnancySMS
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            JobManager.Initialize(new MyRegistry());
+        }
+    }
+
+    public class MyRegistry : Registry
+    {
+
+    }
+
+    internal class DailyAlert : IJob
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void Execute()
+        {
+            
         }
     }
 }
