f747bb47c19866c0a7911d2b4db39a4cf3d98227
cataclysm1987 <conklin.eric@gmail.com>
2017-04-22 11:34:25 -0400

    Add project files.

---
+++ PregnancySMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PregnancySMS.Startup))]
+namespace PregnancySMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
